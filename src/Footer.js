593eee341b051f9acb27ab0cb5e456b6c8669b04
Ona Torres <onatorres@iMac-de-Ona.local>
2018-07-01 12:39:15 +0200

    cambios estilos

--- src/Footer.js
+++ src/Footer.js
@@ -3,27 +3,27 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
       return (
-          <div class = "footer">
-                <div class="adressContact">
-                    <div class="adress">
+          <div className = "footer">
+                <div className="adressContact">
+                    <div className="adress">
                             Dirección <br/>
                             Lorem ipsum dolor, 30<br/>
                             30500 Las Palmas de Gran Canaria<br/>
                             España
                     </div>
-                    <div class="contact">
+                    <div className="contact">
                             Contacto<br/>
                             Email: ipsum@ipsum.es
                     </div>
                 </div>
                 <hr />
-                <div class="help">
+                <div className="help">
                     <a> Accesibilidad</a>
                     <a>Mapa Web</a>
                     <a>Tablon de anuncio</a>
                     <a>Aviso Legal</a>
                     <a>Proteccion de datos</a>
-                    <a>Ayuda</a>
+                    <a className="ayuda">Ayuda</a>
                 </div>
           </div>
       );
